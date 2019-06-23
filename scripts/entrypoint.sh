789557f0f2b3ed962f3f8c92e15c708ba39829f5
Christopher Henry <chenry@mcs.anl.gov>
2019-06-23 02:46:53 -0500

    Fixing data initialization

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -18,7 +18,7 @@ elif [ "${1}" = "async" ] ; then
 elif [ "${1}" = "init" ] ; then
   echo "Initialize module"
   cd /data
-  curl -s http://bioseed.mcs.anl.gov/~chenry/modelseed_reactions.json
+  curl -s http://bioseed.mcs.anl.gov/~chenry/modelseed_reactions.json > /data/modelseed_reactions.json
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
